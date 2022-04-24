138916625c0998179aa16709af418dd48491e1b5
pi <jcho350@gatech.edu>
2022-04-23 23:05:32 -0400

    none

--- scanalgo.py
+++ scanalgo.py
@@ -16,9 +16,9 @@ import RPi.GPIO as GPIO
 import datatools
 
 led = 15
-GPIO.setmode(GPIO.BOARD)
+# GPIO.setmode(GPIO.BOARD)
 GPIO.setup(led, GPIO.OUT)
-GPIO.output(15, True)
+GPIO.output(led, True)
 
 
 class ScanAlgo:
